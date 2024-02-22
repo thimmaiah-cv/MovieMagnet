d84b40f7756c9131203f6be1e8ca8fd79adee70b
Thej48 <chattangadathimmaiah@gmail.com>
2024-02-23 00:06:35 +0530

    Thej Signing Off, Stopping working on config.js file

--- src/Components/MainNav.js
+++ src/Components/MainNav.js
@@ -7,9 +7,20 @@ import WhatshotIcon from '@mui/icons-material/Whatshot';
 import MovieIcon from '@mui/icons-material/Movie';
 import TvIcon from '@mui/icons-material/Tv';
 import SearchIcon from '@mui/icons-material/Search';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function SimpleBottomNavigation() {
     const [value, setValue] = React.useState(0);
+
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        if (value === 0) navigate("/");
+        else if(value === 1) navigate("/movies");
+        else if(value === 2) navigate("/series");
+        else if(value === 3) navigate("/search");
+    }, [value,navigate]);
 
     return (
         <Box sx={{
